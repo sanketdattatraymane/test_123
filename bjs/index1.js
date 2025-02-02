7013f9461660058431be769073f3bd83f3c41263
sanketdattatraymane <139007526+sanketdattatraymane@users.noreply.github.com>
2025-02-02 08:22:00 -0300

    Update index1.js

--- bjs/index1.js
+++ bjs/index1.js
@@ -1,14 +1,14 @@
-// var display=0;
-// $("#menu").click(function(){
-// 		if(display==0){
-// 			$("#menu-card").show();
-// 			display=1;
-// 		}
-// 		else{
-// 			$("#menu-card").hide();
-// 			display=0;			
-// 		}
-// });
+var display=0;
+$("#menu").click(function(){
+		if(display==0){
+			$("#menu-card").show();
+			display=1;
+		}
+		else{
+			$("#menu-card").hide();
+			display=0;			
+		}
+});
 
 // function loadCamp(){
 // 	$.ajax({
@@ -21,22 +21,22 @@
 // 	});
 // }
 
-// $("#m-close").click(function(){
-// 	$("#load-map").hide();
-// });
+$("#m-close").click(function(){
+	$("#load-map").hide();
+});
 
-// $("#add-map").on("click",function(){
-// 	alert("please, search as 'camp{CampID}' for eg. camp5");
-// 	$("#load-map").show();
-// });
+$("#add-map").on("click",function(){
+	alert("please, search as 'camp{CampID}' for eg. camp5");
+	$("#load-map").show();
+});
 
-// $("#map-id").change(function(){
-// 	var id=$("#map-id").val();
-// 	alert("#"+id);
-// 	$("#view-map").empty();
-// 	$("#view-map").append($("#"+id).attr('location'));
-// 	$("#load-map").show();
-// });
+$("#map-id").change(function(){
+	var id=$("#map-id").val();
+	alert("#"+id);
+	$("#view-map").empty();
+	$("#view-map").append($("#"+id).attr('location'));
+	$("#load-map").show();
+});
 
-// loadCamp();
+loadCamp();
 
