40d756abd1f19222667b162946402269f6fee5a9
sanketdattatraymane <139007526+sanketdattatraymane@users.noreply.github.com>
2025-02-02 08:53:39 -0300

    Update index1.js

--- bjs/index1.js
+++ bjs/index1.js
@@ -10,16 +10,90 @@ $("#menu").click(function(){
 		}
 });
 
-function loadCamp(){
-	$.ajax({
-		url:"addCamp.php",
-		type:"GET",
-		success:function(data){
-			$("#drtable").remove();
-			$("#dright").prepend(data);
-		}
-	});
+// function loadCamp(){
+// 	$.ajax({
+// 		url:"addCamp.php",
+// 		type:"GET",
+// 		success:function(data){
+// 			$("#drtable").remove();
+// 			$("#dright").prepend(data);
+// 		}
+// 	});
+// }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+function loadCamp() {
+    $("#drtable").remove(); // Remove existing table if any
+
+    var table = `
+        <table border='1' id='drtable'>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Blood Camp</th>
+                    <th>Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Camp Alpha</td>
+                    <td>2025-02-10</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Camp Beta</td>
+                    <td>2025-02-15</td>
+                </tr>
+            </tbody>
+        </table>
+    `;
+
+    $("#dright").prepend(table); // Add the table to the page
 }
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 $("#m-close").click(function(){
 	$("#load-map").hide();
