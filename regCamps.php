34ceb96659edad70c140c9b06c96843eccf16059
sanketdattatraymane <139007526+sanketdattatraymane@users.noreply.github.com>
2025-02-02 08:29:36 -0300

    Update regCamps.php

--- regCamps.php
+++ regCamps.php
@@ -1,6 +1,6 @@
 <?php
 
-
+/*
 $campName=$_POST["campName"];
 $campDate=$_POST["campDate"];
 $campLoc=$_POST["campLoc"];
@@ -13,5 +13,34 @@ if(mysqli_query($con,$query)){
 else{
 	echo "error";
 }
+*/
+
+
+
+// Retrieve values from POST request
+$campName = $_POST["campName"];
+$campDate = $_POST["campDate"];
+$campLoc = $_POST["campLoc"];
+
+// Generate a random dummy campId (between 1000 and 9999)
+$campId = rand(1000, 9999);
+
+// Establish a connection to the MySQL database
+$con = mysqli_connect("localhost:5000", "root", "root", "bloodbank");
+
+// SQL query to insert values, including the dummy campId
+$query = "INSERT INTO camps(campId, campName, campDate, location) 
+          VALUES('$campId', '$campName', '$campDate', '$campLoc')";
+
+if (mysqli_query($con, $query)) {
+    echo "Added with Dummy ID: " . $campId;
+} else {
+    echo "Error: " . mysqli_error($con);
+}
+
+// Close the database connection
+mysqli_close($con);
+
+
 
 ?>
