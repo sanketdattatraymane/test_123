39a58db469681f06c9d79e247a311df8765dc7ff
sanketdattatraymane <139007526+sanketdattatraymane@users.noreply.github.com>
2025-02-02 07:47:54 -0300

    Update fetch-camps.php

--- fetch-camps.php
+++ fetch-camps.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 $con=mysqli_connect("localhost:5000","root","root","bloodbank");
 
 $rows=mysqli_query($con,"select * from camp");
@@ -18,5 +18,5 @@ $output=$output."<th align='left'><input type='button' onclick=campDelete('".$r[
 else{
 	echo " No records are found !! ";
 }
-
+*/
 ?>
