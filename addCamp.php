8c9c96ef94c22091537130509dab1e8abd031575
sanketdattatraymane <139007526+sanketdattatraymane@users.noreply.github.com>
2025-02-02 08:34:21 -0300

    Update addCamp.php

--- addCamp.php
+++ addCamp.php
@@ -1,25 +1,25 @@
 
 <?php
 
-$con=mysqli_connect("localhost:5000","root","root","bloodbank");
-$query="select * from camps";
-$r=mysqli_query($con,$query);
+// $con=mysqli_connect("localhost:5000","root","root","bloodbank");
+// $query="select * from camps";
+// $r=mysqli_query($con,$query);
 
 if(mysqli_num_rows($r)>0){
 	$table="";
 	$table="<table border='1'  id='drtable'>";
 	$table=$table."<thead><tr><th>ID</th><th style='text-align: center;' class='ff0'> Blood Camp</th><th>Date</th></tr></thead><tbody>";
 
-	while($res=mysqli_fetch_assoc($r)){
-		$table=$table."<tr>";
-		$table=$table."<th>".$res['campId']."</th>";
-		$table=$table."<th>".$res['campName']."</th>";
-		$table=$table."<th id='camp".$res['campId']."' location='".$res['location']."'>".$res['campDate']."</th>";
-		$table=$table."</tr>";
-	}
+	// while($res=mysqli_fetch_assoc($r)){
+	// 	$table=$table."<tr>";
+	// 	$table=$table."<th>".$res['campId']."</th>";
+	// 	$table=$table."<th>".$res['campName']."</th>";
+	// 	$table=$table."<th id='camp".$res['campId']."' location='".$res['location']."'>".$res['campDate']."</th>";
+	// 	$table=$table."</tr>";
+	// }
 
-	$table=$table."</tbody></table>";
-	echo $table;
+	// $table=$table."</tbody></table>";
+	// echo $table;
 }
 
 ?>
